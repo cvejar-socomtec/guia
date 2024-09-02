6f0a47ea1bbcf63e2e8d7a589ac65c8baf6d408b
david <davisvejar@gmail.com>
2024-09-02 17:39:50 -0400

    holamundo final

--- holii/views.py
+++ holii/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Soy el index holii</h1>")
+
+def vista1(request):
+    return HttpResponse("<h1>Soy la vista del holii 1</h1>")
